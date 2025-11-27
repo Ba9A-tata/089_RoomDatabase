86736348c324d5e1005ffa564b1620b29f8e71c9
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-27 13:15:47 +0700

    membuat class OfflineReportSiswa

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -1,10 +1,21 @@
 package com.example.roomdatabase.repositori
 
 import com.example.roomdatabase.room.Siswa
+import com.example.roomdatabase.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream() : Flow<List<Siswa>>
 
     suspend fun  insertSiswa(Siswa: Siswa)
+}
+
+class OfflineReportSiswa(
+    private val SiswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = SiswaDao
+        .getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = SiswaDao
+        .insert(siswa)
 }
