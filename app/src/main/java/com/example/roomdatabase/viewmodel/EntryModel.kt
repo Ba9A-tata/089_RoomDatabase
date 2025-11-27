f1b06fc51195367c8bf2531444aafe6e5e6f6d01
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-27 13:51:23 +0700

    menambahkan data class DetaiSiswa

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryModel.kt
@@ -29,6 +29,13 @@ data class uiStateSiswa(
     val isEntryValid: Boolean = false
 )
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String ="",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
 
 
 }
